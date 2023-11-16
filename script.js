17c7e84c3b630bad04fc112e76e2d4350d05dd35
Mona Lisa <austinnguyenquoc@gmail.com>
2023-11-16 04:03:16 -0400

    add screenshot

--- script.js
+++ script.js
@@ -27,40 +27,42 @@ function writePassword() {
   passwordText.value ="";
 }
 }
-function generatePassword () {
-//I would generate Password based on the prompts
- var password = "";
- for(var i =0; i < characterLength; i++){
-   var randomIndex =Math.floor (Math.random()* choiceArr.length);
-    password = password + choiceArr[randomIndex];
+function generatePassword() {
+  // Generate password based on the prompts
+  var password = "";
+  for (var i = 0; i < characterLength; i++) {
+    var randomIndex = Math.floor(Math.random() * choiceArr.length);
+    password += choiceArr[randomIndex];
   }
   return password;
 }
-function getPrompts () {
 
+function getPrompts() {
   choiceArr = [];
 
+  characterLength = parseInt(prompt("How many characters do you want your password to be? (8-128 characters)"));
 
-  characterLength = parseInt(prompt("how many characters do you and your password to be ? (8-128 characters"));
-
-  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
-    alert("character length has to be a number,8-128 digits. Please try again.");
-    return false; 
+  if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("Character length has to be a number, 8-128 digits. Please try again.");
+    return false;
   }
-  if (confirm("would you like lowercase letters in your password?")) {
+  if (confirm("Would you like lowercase letters in your password?")) {
     choiceArr = choiceArr.concat(lowerCassArr);
   }
-  if (confirm("would you like uppercase letters in your password?")) {
+  if (confirm("Would you like uppercase letters in your password?")) {
     choiceArr = choiceArr.concat(upperCaseArr);
   }
-  if (confirm("would you like special characters in your password?")) {
+  if (confirm("Would you like special characters in your password?")) {
     choiceArr = choiceArr.concat(specialCharArr);
-  }  
-  
-  if (confirm("would you like numbers in your password?")) {
-      choiceArr = choiceArr.concat(numberArr);
-    }
-    return true;
-    
   }
+  if (confirm("Would you like numbers in your password?")) {
+    choiceArr = choiceArr.concat(numberArr);
+  }
+  
+  if (choiceArr.length === 0) {
+    alert("You must select at least one character type for your password!");
+    return false;
+  }
+  
+  return true;
 }
